254cd54c4658383e4cafb52462edcba096e0ef42
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-05-02 17:15:04 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,6 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
+import ListPacks from './pages/ListPacks.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
