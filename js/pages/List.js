1068ac8d1a969d83121c9dbf21a1dadfe9e86004
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-05-27 15:16:56 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -71,7 +71,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="cbf">
-                            <p v-if="record.cbf > 0">{{ record.cbf }}cbf</p>
+                            <p v-if="record.cbf > 0">{{ record.cbf }} </p>
                             </td>
                             <td class="hz">
                                 <p>{{ record.hz }}Hz</p>
