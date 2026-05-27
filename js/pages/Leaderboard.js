9ea5a9b1b6246b473489db352e5956b4c7cd528e
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-05-27 15:32:00 +0200

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -55,7 +55,7 @@ export default {
                                     <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
                                 <td class="cbf">
-                                    <p>#{{ record.cbf }}</p>
+                                    <p>#{{ score.cbf }}</p>
                                 </td>
                                 <td class="score">
                                     <p>+{{ localize(score.score) }}</p>
