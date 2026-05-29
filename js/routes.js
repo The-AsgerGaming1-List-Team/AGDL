a23af3a27dde0a05eb01f2e1b1c9c6a3579bb282
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-05-29 21:11:13 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,13 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import ListPacks from './pages/ListPacks.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/packs', component: ListPacks },
+    { path: '/listpacks', component: ListPacks },
+    { path: '/:pathMatch(.*)*', component: List },
 ];
