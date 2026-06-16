cb19380b53d80ceef6fd11884ed9e14fde34caa1
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-06-16 23:37:05 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -54,6 +54,10 @@ export default {
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
+                        <li>
+                            <div class="type-title-sm">Verifier used cbf?</div>
+                            <p>{{ level.cbf }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 40"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
