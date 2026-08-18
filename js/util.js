5c2c80d3e8b2cdb0f2275cc461f81ad39b73ec04
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-08-18 14:55:35 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -5,6 +5,18 @@ export function getYoutubeIdFromUrl(url) {
     )?.[1] ?? '';
 }
 
+export function getFontColour(background) {
+    const hex = background.replace("#", "");
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+    return brightness > 128 ? "#000" : "#fff";
+}
+
 export function embed(video) {
     return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}`;
 }
