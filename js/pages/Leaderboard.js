9344706275e60c1b36dada7d4f646ed6433fd915
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-08-18 20:58:40 +0200

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -46,16 +46,20 @@ export default {
                     <div class="player">
                         <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
                         <h3>{{ entry.total }}</h3>
-<div v-if="entry.packs && entry.packs.length" class="player-packs">
+<div v-if="entry.packs && entry.packs.length">
     <h2>Packs</h2>
 
-    <div class="player-packs-list">
+    <div>
         <span
             v-for="pack in entry.packs"
-            class="player-pack"
             :style="{
                 backgroundColor: pack.colour,
-                color: getFontColour(pack.colour)
+                color: getFontColour(pack.colour),
+                display: 'inline-block',
+                padding: '6px 10px',
+                marginRight: '8px',
+                marginBottom: '8px',
+                borderRadius: '6px'
             }"
         >
             {{ pack.name }}
