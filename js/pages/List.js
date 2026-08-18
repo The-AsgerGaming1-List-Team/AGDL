13b8ee7833c177fec228141d3b56bc3055a1caee
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-08-18 19:43:42 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -42,13 +42,12 @@ export default {
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
 
                     <div v-if="level.packs?.length" class="packs">
-                        <a
+                        <div
                             v-for="pack in level.packs"
                             class="pack"
                             :style="'background-color: ' + pack.colour + '; color: ' + getFontColour(pack.colour)"
-                    :href="'/packs?pack=' + encodeURIComponent(pack.name)">
                             {{ pack.name }}
-                        </a>
+                        </div>
                     </div>
 
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
