d386146e1c5a20d24fc74068e7ab41705f25b9d5
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-08-23 09:34:39 +0200

    Update OpenVerifications.js

--- js/pages/OpenVerifications.js
+++ js/pages/OpenVerifications.js
@@ -61,6 +61,10 @@ export default {
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
+                        <li>
+                            <div class="type-title-sm">Estimated placement</div>
+                            <p>{{ level.est }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 40"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
