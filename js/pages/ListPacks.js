fed9068840112412058bbcae4818983b4c1f3c94
AsgerGaming1 <134543383+AsgerGaming1@users.noreply.github.com>
2026-09-23 09:09:43 +0200

    Update ListPacks.js

--- js/pages/ListPacks.js
+++ js/pages/ListPacks.js
@@ -48,7 +48,7 @@ export default {
                             <p class="type-label-lg">#{{ i + 1 }}</p>
                         </td>
                         <td class="level" :class="{ 'active': selectedLevel == i, 'error': !level || !level[0] }">
-                            <button :style="selectedLevel === i && pack ? { background: pack.colour } color: getFontColour(pack.colour) : {}" @click="selectedLevel = i" >
+                            <button :style="selectedLevel === i && pack ? { background: pack.colour } : {}" @click="selectedLevel = i" >
                                 <span class="type-label-lg">
                                     {{ (level && level[0] && level[0].level && level[0].level.name) ? level[0].level.name : ('Error (' + (level && level[1] ? level[1] : '?') + '.json)') }}
                                 </span>
